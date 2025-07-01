93fa810370c2c39438baf44921168c61ecf3c8e3
Sonu1234-sonu <sonulovewanshi999@gmail.com>
2025-07-01 20:54:52 +0530

    connection Database

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from 'express';
+import connectDB from "./src/config/db.js";
 import AuthRouter from "./src/routes/authRouter.js";
 const app=express();
  app.use("/auth",AuthRouter);
@@ -6,9 +9,18 @@ app.get("/",(request,response)=>{
     response.json({message:"Mai server hoon"})
 })
 
+// let port;
+// if(process.env.PORT)
+// {
+//     port=process.env.PORT;
+// }
+// else{
+//     port=5000;
+// }
+const port=process.env.PORT||5000;
 
-
-
- app.listen(5000,()=>{
-    console.log("Server start ho gya 5000 port pe")
+// const port=process.env.PORT;
+ app.listen(port,()=>{
+    console.log("Server Started at ",port);
+    connectDB();
  })
